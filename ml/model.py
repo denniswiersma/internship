b2682624450cfa1e194902987f27040fac71d36f
denniswiersma <dennis.wiersma@icloud.com>
2023-11-08 10:59:26 +0100

    replaced path validation method w/ one-liner

--- ml/model.py
+++ ml/model.py
@@ -26,21 +26,6 @@ class Model(ABC):
         ...
 
     @staticmethod
-    def _validate_path(path: Path):
-        """
-        Checks if a given path exists, and creates it if it does not.
-
-        :param path: the path to a file. Please note this does not work with
-        directories, since the parents of the path are created, not the path.
-        """
-        if not path.parent.exists():
-            print(f"Path {path.parent} does not exist. Creating it now.")
-            parent_dirs = path.parents
-            for parent_dir in reversed(parent_dirs):
-                if not parent_dir.exists():
-                    parent_dir.mkdir()
-
-    @staticmethod
     def _generate_runID() -> str:
         return secrets.token_hex(3)
 
@@ -59,7 +44,7 @@ class Model(ABC):
 
     def save(self, path: Path):
         path = path.with_suffix(".pkl")
-        self._validate_path(path)
+        path.parent.mkdir(parents=True)
 
         with open(path, "wb") as file:
             pkl.dump(self.fitted_model, file=file)
