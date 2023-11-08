0630395e29925931a8f53db6b357c7fcc4d802b3
denniswiersma <dennis.wiersma@icloud.com>
2023-11-08 12:53:37 +0100

    implemented model's save method

--- ml/ctree.py
+++ ml/ctree.py
@@ -8,7 +8,6 @@
 # IMPORTS
 import dataclasses
 import math
-import pickle as pkl
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -27,6 +26,7 @@ class Ctree(Model):
         self.tumor_types: pd.DataFrame = data.tumor_types
         self.mm_with_tt: pd.DataFrame = data.get_mm_with_tt()
         self.r_mm_with_tt = data.get_r_mm_with_tt()
+        self.data = data
 
     def _build_formula(
         self,
@@ -47,7 +47,7 @@ class Ctree(Model):
         return model_formula
 
     @dataclass
-    class ctree_control:
+    class CtreeControl:
         testtype: str = "Bonferroni"  # how to compute the distribution of the test statistic
         alpha: float = 0.05  # significance level for variable selection
         maxdepth: float | int = math.inf  # maximum depth of the tree
@@ -56,12 +56,13 @@ class Ctree(Model):
 
     def fit(
         self,
-        ctree_control: ctree_control,
+        ctree_control: CtreeControl,
         predictors: list = ["consensus independent component 1"],
         # TODO: because of train_test_split, a given ncolumns will only be included in the dataset
         # therefore, the formula should be constructed based on all the columns in the passed dataset
         response: str = "response",
     ):
+        self.ctree_control = ctree_control
         # TODO: add pydoc
         model_formula = self._build_formula(predictors.copy(), response)
 
@@ -82,82 +83,61 @@ class Ctree(Model):
             control=control,
         )
 
+        self.fitted_model = model
+        self.runID = self._generate_runID()
+
         return model
 
     # TODO: implement plot function that returns figure and axes
     # and let the user save the figure themselves from the ipynb.
     # save_tree shall only save the serialised model to disk.
 
-    def save(
-        self,
-        model,
-        ctree_control: ctree_control,
-        predictors: list = ["consensus independent component 1"],
-        type: str | list[str] = ["img", "model"],
-    ):
-        # TODO: add pydoc
+    def predict(self, newx):
+        pass
 
-        # construct a string describing the tree's settings
+    def assess(self, ytrue, ypred_proba):
+        pass
+
+    def save(self):
+        # fetch the output dir for ctree
+        output_dir = Path(self.data.config["output"]["locations"]["ctree"])
+        # append the runID as a subfolder
+        output_dir = output_dir.joinpath(self.runID)
+
+        # construct a file name describing the tree's settings
         file_name = "ctree"
-        for key, value in dataclasses.asdict(ctree_control).items():
+        for key, value in dataclasses.asdict(self.ctree_control).items():
             file_name += f"-{key}={str(value).replace('.', '_')}"
 
-        # construct path to file based on predictors
-        # TODO: don't do these folders anymore, perhaps do generate a run ID.
-        if len(predictors) > 1:
-            file_path = Path(
-                f"ml/{predictors[0]}_{predictors[-1]}/" + file_name
-            )
-        else:
-            file_path = Path(f"ml/{predictors[0]}/" + file_name)
+        # append the file name to the output dir
+        output_dir = output_dir.joinpath(file_name)
 
-        if not file_path.parent.exists():
-            file_path.parent.mkdir()
+        # save the model as a pickle file
+        super().save(path=output_dir)
 
-        def save_image(file_path):
-            # append file extension
-            file_path = file_path.with_suffix(".png")
-            # import the graphics device in order to enable saving images to
-            # disk
-            grdevices = importr("grDevices")
-            grdevices.png(
-                file=file_path.as_posix(),
-                width=5000,
-                height=1500,
-            )
+    def plot(self, file_path):
+        # append file extension
+        file_path = file_path.with_suffix(".png")
+        # import the graphics device in order to enable saving images to
+        # disk
+        grdevices = importr("grDevices")
+        grdevices.png(
+            file=file_path.as_posix(),
+            width=5000,
+            height=1500,
+        )
 
-            # plot the tree and save to disk
-            robjects.r.plot(  # type: ignore
-                model,
-                margins=robjects.r.list(15, 0, 0, 0),  # type: ignore
-                tp_args=robjects.r.list(  # type: ignore
-                    rot=90, just=robjects.r.c("right", "top")  # type: ignore
-                ),
-            )
-            # disable graphics device
-            grdevices.dev_off()
-            print(f"saved image at {file_path}")
-
-        def save_model(file_path):
-            # append file extension
-            file_path = file_path.with_suffix(".pkl")
-
-            with open(file_path, "wb") as file:
-                pkl.dump(model, file=file)
-            print(f"saved model at {file_path}")
-
-        match type:
-            case "img":
-                save_image(file_path)
-            case "model":
-                save_model(file_path)
-            case ["img", "model"] | ["model", "img"]:
-                save_image(file_path)
-                save_model(file_path)
-            case _:
-                raise ValueError(
-                    f"{type} not of valid values 'img' 'model' ['img', 'model']"
-                )
+        # plot the tree and save to disk
+        robjects.r.plot(  # type: ignore
+            self.fitted_model,
+            margins=robjects.r.list(15, 0, 0, 0),  # type: ignore
+            tp_args=robjects.r.list(  # type: ignore
+                rot=90, just=robjects.r.c("right", "top")  # type: ignore
+            ),
+        )
+        # disable graphics device
+        grdevices.dev_off()
+        print(f"saved image at {file_path}")
 
 
 # FUNCTIONS
